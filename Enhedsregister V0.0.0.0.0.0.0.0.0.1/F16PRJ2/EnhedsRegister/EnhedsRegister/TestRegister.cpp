d7565cd79b5c832c22b3630ad720cd2d189c68e3
Topping <Topping@users.noreply.github.com>
2016-05-17 20:32:38 +0200

    min gren... Den er lang

--- Enhedsregister V0.0.0.0.0.0.0.0.0.1/F16PRJ2/EnhedsRegister/EnhedsRegister/TestRegister.cpp
+++ Enhedsregister V0.0.0.0.0.0.0.0.0.1/F16PRJ2/EnhedsRegister/EnhedsRegister/TestRegister.cpp
@@ -4,13 +4,13 @@ int main()
 {
 	unsigned char c = 0x00;
 	EnhedsRegister m_register;
-/*	for (int i = 0; i <= 20; ++i)
+	m_register.getStoredUnits();
+	for (int i = 0; i <= 20; ++i)
 	{
 		c++;
 		if (!m_register.storeUnit(unit(c, 0x00, false, 0)))
 			cout << "Failed to add unit" << endl;
-	}*/
-	m_register.getStoredUnits();
+	}
 	/*ofstream f("test.txt");
 	ostream_iterator<unit> oiter(f, "\n");
 	copy(testVector.begin(), testVector.end(), oiter);*/

24c2b424267982e553e5846d8040354928e969c1
AndersBKnudsen <Andersknudsen93@gmail.com>
2016-05-20 23:07:02 +0200

    Tid til at rive lidt i banditten

--- HomeAutomation/HomeAutomation/addunit.cpp
+++ HomeAutomation/HomeAutomation/addunit.cpp
@@ -1,5 +1,18 @@
 #include "addunit.h"
 #include "ui_addunit.h"
+#include <QInputDialog>
+#include <QString>
+//========================================================================
+// FILENAME : <addUnit.h>
+// CREATED : <20/05-2016>
+// AUTHOR : <Anders Brondbjerg Knudsen>
+// DESCR. : <Header filen for addUnit>
+//
+//------------------------------------------------------------------------
+//
+// REV. DATE/AUTHOR CHANGE DESCRIPTION
+// 1.0 <20-05/2016/Anders Brondbjerg Knudsen> <Oprettelse af addUnit>
+//========================================================================
 
 AddUnit::AddUnit(QStackedWidget *parent, UnitRegister &regRef, CommInterface &comRef) :
     QWidget(parent),
@@ -14,4 +27,26 @@ AddUnit::AddUnit(QStackedWidget *parent, UnitRegister &regRef, CommInterface &co
 AddUnit::~AddUnit()
 {
     delete ui;
+}
+
+void AddUnit::on_pushButton_clicked()
+{
+
+  int xvalue=ui->spinBox->value();
+  Unit tempUnit(xvalue,1,2,1);
+
+  getRegistryPtr()->storeUnit(tempUnit); //Filen findes ikke ?
+}
+
+
+void AddUnit::on_pushButton_3_clicked()
+{
+    QString N = QInputDialog::getText(this,"Tilføj nyt rum","Tilføj rum");
+    ui->comboBox->addItem(N);
+
+}
+
+void AddUnit::on_pushButton_2_clicked()
+{
+    getParentPtr()->setCurrentIndex(0);
 }
